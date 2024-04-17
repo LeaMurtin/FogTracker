802417906c75189ac63540cd4635d691cbb219e3
Woli <>
2024-04-17 20:40:40 +0200

    add react-hook eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,22 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import hooksPlugin from 'eslint-plugin-react-hooks';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   { ...pluginReactConfig, settings: { react: { version: 'detect' } } },
+  {
+    plugins: {
+      'react-hooks': hooksPlugin,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  },
   {
     rules: {
       curly: 'error',
