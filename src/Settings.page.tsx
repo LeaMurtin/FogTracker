dc34d502e18836da988f41b35cb8ec955745e37a
lea <bitbucket>
2024-04-17 18:43:50 +0200

    trucs

--- src/Settings.page.tsx
+++ src/Settings.page.tsx
@@ -1,32 +1,46 @@
-import { Group, Image, Tabs, Box, Button, TextInput } from '@mantine/core';
-import { useEffect, useState } from 'react';
-import { isEqual } from 'lodash';
-import SettingsPanel from './components/SettingsPanel';
-import settingList from './settingList';
+import { Group, Image, Tabs, Box, Button, TextInput } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { isEqual } from "lodash";
+import SettingsPanel from "./components/SettingsPanel";
+import settingList from "./settingList";
+
+function compareArrayValues(a: string[], b: string[]) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  return (
+    a.every((entry) => b.includes(entry)) &&
+    b.every((entry) => a.includes(entry))
+  );
+}
 
 export default function SettingsPage() {
-  const [settingString, setSettingString] = useState('');
-  const [settingValues, setSettingValues] = useState<Record<string, boolean>>({});
+  const [settingString, setSettingString] = useState("");
+  const [settingValues, setSettingValues] = useState<Record<string, boolean>>(
+    {}
+  );
 
   useEffect(() => {
     // parse all settings from string
     // check for invalid string and extract values
     // setState new set of values
-    const parsedSettings = settingString.split(' ');
+    const parsedSettings = settingString.split(" ");
     const newSettingValues: Record<string, boolean> = {};
-    const shuffle = parsedSettings.includes('shuffle');
-    const crawl = parsedSettings.includes('crawl');
-    if (shuffle === crawl ) {
+    const shuffle = parsedSettings.includes("shuffle");
+    const crawl = parsedSettings.includes("crawl");
+    if (shuffle === crawl) {
       // TODO : envoyer message d'erreur <INVALID SETTING STRING>
+      return;
     }
-    const mode = shuffle ? 'shuffle' : 'crawl';
-    parsedSettings.forEach(setting => {
+    const mode = shuffle ? "shuffle" : "crawl";
+    parsedSettings.forEach((setting) => {
       if (settingList[setting]) {
         newSettingValues[`${mode}-${setting}`] = true;
       }
     });
+    console.log(settingValues, newSettingValues);
     if (!isEqual(settingValues, newSettingValues)) {
-      console.log('COUCOU MAMAN JE PASSE A LA TELE');
+      console.log("COUCOU MAMAN JE PASSE A LA TELE");
       setSettingValues(newSettingValues);
     }
   }, [settingString]);
@@ -34,32 +48,39 @@ export default function SettingsPage() {
   useEffect(() => {
     // parse all settings from string
     // si ya un setting qui est different entre string et values, setState string
-    const parsedSettings = settingString.split(' ');
+    const parsedSettings = settingString.split(" ");
     let newSettings: string[] = [];
-    const mode = 'shuffle'; //detecter tab actif
-    parsedSettings.forEach(setting => {
+    const mode = "shuffle"; //detecter tab actif
+    parsedSettings.forEach((setting) => {
       if (!settingList[setting]) {
         newSettings.push(setting);
-      } else 
-      if (settingList[setting] && settingValues[`${mode}-${setting}`]) {
+      } else if (settingList[setting] && settingValues[`${mode}-${setting}`]) {
         newSettings.push(setting);
       }
     });
     for (const setting in settingList) {
-      if (settingValues[`${mode}-${setting}`] && !newSettings.includes(setting)) {
+      if (
+        settingValues[`${mode}-${setting}`] &&
+        !newSettings.includes(setting)
+      ) {
         newSettings.push(setting);
       }
     }
-    if (!isEqual(parsedSettings, newSettings)) {
-      console.log('LE PLUS BEAU CEST WIWI');
-      const newSettingString = newSettings.join(' ');
+    if (!compareArrayValues(newSettings, parsedSettings)) {
+      // console.log("LE PLUS BEAU CEST WIWI");
+      const newSettingString = newSettings.join(" ");
       setSettingString(newSettingString);
     }
   }, [settingValues]);
 
   return (
     <>
-      <Image radius="md" src={null} h={100} fallbackSrc="https://placehold.co/600x400?text=Placeholder" />
+      <Image
+        radius="md"
+        src={null}
+        h={100}
+        fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+      />
       <Box mt="md">
         <Group justify="center">
           Setting string
@@ -77,10 +98,18 @@ export default function SettingsPage() {
             <Tabs.Tab value="crawl">Dungeon Crawl</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel value="shuffle">
-            <SettingsPanel mode="shuffle" settingValues={settingValues} setSettingValues={setSettingValues} />
+            <SettingsPanel
+              mode="shuffle"
+              settingValues={settingValues}
+              setSettingValues={setSettingValues}
+            />
           </Tabs.Panel>
           <Tabs.Panel value="crawl">
-            <SettingsPanel mode="crawl" settingValues={settingValues} setSettingValues={setSettingValues} />
+            <SettingsPanel
+              mode="crawl"
+              settingValues={settingValues}
+              setSettingValues={setSettingValues}
+            />
           </Tabs.Panel>
         </Tabs>
         <Group justify="center">
